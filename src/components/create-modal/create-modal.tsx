b87505e484e934b6edbf8d1abe2be54b1caaddf6
henriqueAlgauer <algauer.henrique@gmail.com>
2024-05-12 23:59:34 -0300

    first commit

---
+++ src/components/create-modal/create-modal.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useFoodDataMutate } from "../../hooks/useFoodDataMutate";
+import { FoodData } from "../../interface/FoodData";
+import "./modal.css";
+
+interface InputProps {
+  label: string;
+  value: string | number;
+  updateValue: (value: any) => void;
+}
+const Input = ({ label, value, updateValue }: InputProps) => {
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        value={value}
+        type="text"
+        onChange={(event) => updateValue(event.target.value)}
+      />
+    </>
+  );
+};
+
+interface ModalProps {
+  closeModal(): void;
+}
+
+export function CreateModal({ closeModal }: ModalProps) {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const { mutate, isSuccess, isPending } = useFoodDataMutate();
+
+  const submit = () => {
+    const foodData: FoodData = {
+      title,
+      price,
+      image,
+    };
+    mutate(foodData);
+  };
+
+  useEffect(() => {
+    if (!isSuccess) return;
+    closeModal();
+  }, [isSuccess]);
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-body">
+        <h2>Cadastre um novo produto no cardápio</h2>
+        <form className="intput-container">
+          <Input label="title" value={title} updateValue={setTitle}></Input>
+          <Input label="price" value={price} updateValue={setPrice}></Input>
+          <Input label="image" value={image} updateValue={setImage}></Input>
+        </form>
+        <button onClick={submit} className="btn-secondary">
+          {isPending ? "postando... " : "postar"}
+        </button>
+      </div>
+    </div>
+  );
+}
